175e6ac8b7e4e5ea2ffbb7a4dd9e4d5d65eb772b
Grzegorz Galios <grzegorz.galios@wp.pl>
2019-04-06 11:51:00 +0200

    Program przygotowany do napisania algorytmu

--- pasjans.cc
+++ pasjans.cc
@@ -1,98 +1,102 @@
-#include <algorithm>
+#include <vector>
+#include <utility>
+#include <string>
 #include <chrono>
 #include <iostream>
-#include <memory>
-#include <string>
 #include <sstream>
-#include <vector>
 
-unsigned playSolitaire(
-		unsigned sizeOfSeries,
-		const std::vector<unsigned> &series);	
-std::vector<std::vector<unsigned>> createPermutations(
-		unsigned sizeOfSeries);
-std::vector<unsigned> computeAllResults(
-		const std::vector<std::vector<unsigned>> &permutations,
-		const std::vector<unsigned> &series);
-unsigned computeSingleResult(
-		const std::vector<unsigned> &permutation,
-		std::vector<unsigned> series);		
-unsigned findMaxResult(std::vector<unsigned> results);
+#define PRINT_DEBUG 0
 
+// Type aliases
+using std::vector;
+using std::pair;
+using DataRow = pair<size_t, vector<unsigned>>;
+using InputData = vector<DataRow>;
+using Clock = std::chrono::high_resolution_clock;
+using TimeMeasure = std::chrono::duration<double>;
+using ProgramResults = vector<unsigned>;
+
+// I/O functions
+InputData getInputData();
+
+// Debug purpose functions
+void print(const InputData &inputData);
+
+// Exact algorithm's functions
+ProgramResults playSolitaire(const InputData &inputData);
+unsigned calculateResult(const DataRow &dataRow);
+void printResults(const ProgramResults &results);
+
+// main
 int main() {
-	auto beginTime = std::chrono::high_resolution_clock::now();
-	std::string inputLine;
-	while (std::getline(std::cin, inputLine)) {
-		unsigned element = 0, sizeOfSeries = 0;
-		std::vector<unsigned> series;
-		std::istringstream inputStream(inputLine);
-		inputStream >> sizeOfSeries;
-		
-		if (sizeOfSeries < 3) {
-			break;
-		}
-		while(inputStream >> element) {
-			series.push_back(element);
-		}
-		
-		unsigned result = playSolitaire(sizeOfSeries, series);
-		std::cout << "result: " << result << std::endl;
-	}
-	auto endTime = std::chrono::high_resolution_clock::now();
-	std::chrono::duration<double> executionTime = endTime - beginTime;
+	auto inputData = getInputData();
+	
+	#if PRINT_DEBUG == 1
+	print(inputData);
+	#endif
+	
+	auto beginTime = Clock::now();
+	auto results = playSolitaire(inputData);
+	auto endTime = Clock::now();
+	printResults(results);
+	
+	TimeMeasure executionTime = endTime - beginTime;	
 	std::cout << "Execution time: " << executionTime.count()
 			<< " seconds." << std::endl;
 	return EXIT_SUCCESS;
 }
 
-unsigned playSolitaire(
-		unsigned sizeOfSeries,
-		const std::vector<unsigned> &series) {
-	unsigned maxResult = 0;
-	unsigned currentResult = 0;
-
-	std::vector<unsigned> currentPermutation;
-	unsigned i = 1;
-	while (i < sizeOfSeries-1) {
-		currentPermutation.push_back(i);
-		++i;
-	}
-	
-	do {
-		currentResult = computeSingleResult(
-				currentPermutation,
-				series);
-		if (currentResult > maxResult) {
-			maxResult = currentResult;
+InputData getInputData() {
+	std::string inputLine;
+	InputData inputData;
+	while (std::getline(std::cin, inputLine) && !inputLine.empty()) {
+		std::istringstream inputStream(inputLine);
+		size_t sequenceSize;
+		inputStream >> sequenceSize;
+		
+		if (sequenceSize == 0) {
+			break;
 		}
-	} while (std::next_permutation(
-				currentPermutation.begin(),
-				currentPermutation.end())
-			);
-
-	return maxResult;
+		
+		auto sequence = vector<unsigned>(sequenceSize);
+		unsigned sequenceElement = 0;
+		size_t sequenceIndex = 0;
+		
+		while (inputStream >> sequenceElement) {
+			sequence.at(sequenceIndex++) = sequenceElement;
+		}		
+		inputData.push_back(std::move(DataRow(sequenceSize, sequence)));
+	}
+	return std::move(inputData);
 }
 
-unsigned computeSingleResult(
-		const std::vector<unsigned> &permutation,
-		std::vector<unsigned> series) {
-	unsigned result = 0;
-	
-	for (unsigned index : permutation) {
-		result += series.at(index);
-		series.at(index) = 0;
-
-		unsigned leftIndex = index - 1;		
-		while (series.at(leftIndex) == 0) {
-			--leftIndex;
+void print(const InputData &inputData) {
+	for (const auto &row: inputData) {
+		std::cout << row.first << " ";
+		for (const auto element : row.second) {
+			std::cout << element << " ";
 		}
-		result += series.at(leftIndex);
-		
-		unsigned rightIndex = index + 1;
-		while (series.at(rightIndex) == 0) {
-			++rightIndex;
-		}
-		result += series.at(rightIndex);
+		std::cout << "\n";
 	}
-	return result;
+}
+
+ProgramResults playSolitaire(const InputData &inputData) {
+	const size_t inputSize = inputData.size();
+	ProgramResults results(inputSize);
+	
+	for (size_t i = 0; i != inputSize; ++i) {
+		unsigned result = calculateResult(inputData[i]);
+		results[i] = result;
+	}
+	return std::move(results);
+}
+
+unsigned calculateResult(const DataRow & /*dataRow*/) {
+	return 0;
+}
+
+void printResults(const ProgramResults &results) {
+	for (auto result : results) {
+		std::cout << result << "\n";
+	}
 }
